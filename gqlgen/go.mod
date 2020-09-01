e88184953fb0ad90cf4a7b931160208ab908ebf9
tle-dieu arch <thomas.lddv@gmail.com>
2020-09-01 14:16:23 +0200

    mysql migration

--- gqlgen/go.mod
+++ gqlgen/go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/99designs/gqlgen v0.12.2
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/golang-migrate/migrate/v4 v4.12.2
 	github.com/vektah/gqlparser/v2 v2.0.1
 )
--- gqlgen/server.go
+++ gqlgen/server.go
@@ -3,27 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/tle-dieu/gql_test/gqlgen/graph"
 	"github.com/tle-dieu/gql_test/gqlgen/graph/generated"
+	"github.com/tle-dieu/gql_test/gqlgen/pkg/db/mysql"
 )
 
 const defaultPort = "8080"
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
+	mysqlClient := mysql.NewMySQLClient()
+	mysqlClient.Migrate()
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
+	port := "8080"
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
